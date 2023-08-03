792cf8d77dc6b842f6bd19664ce09ac9ef44f186
langningc2009 <jpxx_chenlangning@outlook.com>
2023-08-03 09:50:56 +0800

    Fix

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      0.1.3
+// @version      0.1.4
 // @description  Test
 // @author       @langningchen
 // @match        http://www.xmoj.tech/*
