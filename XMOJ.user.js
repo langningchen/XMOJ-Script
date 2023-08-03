5455e8e9d0b2823a22c2badd8992d9f8ca381a2b
langningc2009 <jpxx_chenlangning@outlook.com>
2023-08-03 09:44:11 +0800

    Commit

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      0.1.1
+// @version      0.1.3
 // @description  Test
 // @author       @langningchen
 // @match        http://www.xmoj.tech/*
