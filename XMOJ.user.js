36cbdea5f48162ec7df43caca3dd0959696c0f42
langningchen <jpxx_chenlangning@outlook.com>
2023-08-07 02:46:07 +0000

    Update version to 0.1.28

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      0.1.27
+// @version      0.1.28
 // @description  XMOJ增强脚本
 // @author       @langningchen
 // @namespace    https://github/langningchen
