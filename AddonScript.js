5e4cb6c635a4c20c028ae4229f3d0e4d8c1e4d20
zcr <85378277+boomzero@users.noreply.github.com>
2023-08-14 16:34:59 +0800

    format

--- AddonScript.js
+++ AddonScript.js
@@ -1,4 +1,4 @@
-if (localStorage.getItem("UserScript-ImportantNotice-20230814")==null) {
+if (localStorage.getItem("UserScript-ImportantNotice-20230814") == null) {
     alert("警告！警告！警告！请仔细阅读以下内容！如果您的用户脚本在北京时间2023/08/14 15:10左右更新到了0.1.47，请按确定后手动升级!");
     location.href = "https://github.com/langningchen/XMOJ-Script/releases/download/0.1.49/XMOJ.user.js"
     localStorage.setItem("UserScript-ImportantNotice-20230814", "true")
