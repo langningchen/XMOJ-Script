1a065a15d99101479b5ef61ded529ca3dcc90df9
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2023-08-26 22:20:11 +0800

    save stuff

--- email-server/xmoj-email/src/worker.ts
+++ email-server/xmoj-email/src/worker.ts
@@ -1,32 +1,38 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	db: D1Database;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		if (request.method != 'POST') {
+			return new Response(JSON.stringify({ sucuess: false, error: 'Invalid request method' }), {
+				status: 400,
+				headers: { 'Content-Type': 'application/json' },
+			});
+		}
+		//save the request body as a string
+		let body:JSON = await request.json();
+		const email:string = body["email"], username:string = body["username"];
+		//check if any of the required fields are missing
+		if (!body["email"]|| !body["username"]) {
+			return new Response(JSON.stringify({ sucuess: false, error: 'Missing required fields' }), {
+				status: 400,
+				headers: { 'Content-Type': 'application/json' },
+			});
+		}
+		//use regex to check whether body is a valid email
+		const emailRegex = new RegExp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$');
+		if (!emailRegex.test(email)) {
+			return new Response(JSON.stringify({ sucuess: false, error: 'Invalid email' }), {
+				status: 400,
+				headers: { 'Content-Type': 'application/json' },
+			});
+		}
+		//save the email to the database
+		await env.db.prepare('INSERT INTO email (userID, emailHash) VALUES (?1, ?2)').bind(email, username).run();
+		return new Response(JSON.stringify({ sucuess: true }), {
+			status: 200,
+			headers: { 'Content-Type': 'application/json' },
+		});
 	},
 };
