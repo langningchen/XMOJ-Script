2db2669f9b124401a336c85f642f30abcc1b4517
langningc2009 <jpxx_chenlangning@outlook.com>
2023-09-02 15:28:59 +0800

    Revert "Update version info to 0.2.93"
    This reverts commit b1aa4f8b96c9eb3e50d04c46b265731e93573fee.

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      0.2.93
+// @version      0.2.92
 // @description  XMOJ增强脚本
 // @author       @langningchen
 // @namespace    https://github/langningchen
