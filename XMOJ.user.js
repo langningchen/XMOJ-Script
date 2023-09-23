68d858759edf72de2fc8526fec83d13c7218c5f2
Chen LangNing <jpxx_chenlangning@outlook.com>
2023-09-23 05:26:45 +0000

    [Feature Request] 增加清除缓存功能 Fixes #435

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -291,6 +291,18 @@ let RequestAPI = (Action, Data, CallBack) => {
     });
 };
 
+GM_registerMenuCommand("清除缓存", () => {
+    let Temp = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i).startsWith("UserScript-User-")) {
+            Temp.push(localStorage.key(i));
+        }
+    }
+    for (let i = 0; i < Temp.length; i++) {
+        localStorage.removeItem(Temp[i]);
+    }
+    location.reload();
+});
 GM_registerMenuCommand("重置数据", () => {
     if (confirm("确定要重置数据吗？")) {
         localStorage.clear();
