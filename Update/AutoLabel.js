792204ea077f9fc322d71702568f71e23a2d9336
Chen LangNing <jpxx_chenlangning@outlook.com>
2023-09-23 03:06:42 +0000

    This may good!

--- Update/AutoLabel.js
+++ Update/AutoLabel.js
@@ -1,4 +1,9 @@
 import * as github from '@actions/github';
+
+const IgnoreUsers = [
+    "cloudflare-pages[bot]"
+];
+
 let Data = github.context.payload.comment.body;
 let Octokit = github.getOctokit(process.argv[2]);
 let Owner = github.context.repo.owner;
@@ -12,17 +17,25 @@ console.log("Repo       : " + Repo);
 console.log("IssueNumber: " + IssueNumber);
 console.log("CommentID  : " + CommentID);
 console.log("User       : " + User);
+
+if (IgnoreUsers.includes(User)) {
+    console.log("Ignore user " + User);
+    return;
+}
+
 Data = Data.replaceAll(/\/[a-zA-Z-]+/g, (match) => {
     let Label = match.substring(1);
     console.log("Add label " + Label);
-    // Octokit.issues.addLabels({
-    //     owner: Owner,
-    //     repo: Repo,
-    //     issue_number: IssueNumber,
-    //     labels: [Label]
-    // });
+    Octokit.issues.addLabels({
+        owner: Owner,
+        repo: Repo,
+        issue_number: IssueNumber,
+        labels: [Label]
+    });
     return "";
 });
+
+console.log("New Data   : " + Data);
 Octokit.issues.updateComment({
     owner: Owner,
     repo: Repo,
