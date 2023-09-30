c37dca677513c67e203f7fcf246d2df91363e6b2
Chen LangNing <jpxx_chenlangning@outlook.com>
2023-09-30 14:03:44 +0800

    Resolve conflict

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      0.3.184
+// @version      0.3.185
 // @description  XMOJ增强脚本
 // @author       @langningchen
 // @namespace    https://github/langningchen
